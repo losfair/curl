cd01ca24111137998b97e8e682d3b3a8f81c43ad
losfair <zhy20000919@hotmail.com>
2020-10-13 16:14:01 +0800

    Configuration files.

---
+++ run_configure.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export AR="/opt/wasi-sdk/bin/ar"
+export AS="/opt/wasi-sdk/bin/as"
+export CC="/opt/wasi-sdk/bin/clang"
+export LD="/opt/wasi-sdk/bin/ld.lld"
+export RANLIB="/opt/wasi-sdk/bin/ranlib"
+export CFLAGS="-Wl,--allow-undefined-file=$PWD/wasio-symbols.txt"
+#export CPPFLAGS=""
+
+./configure --host wasm32 \
+    --disable-shared --disable-thread --disable-threaded-resolver \
+    --without-libpsl --disable-netrc --disable-unix-sockets \
+    --disable-ipv6 --disable-tftp
